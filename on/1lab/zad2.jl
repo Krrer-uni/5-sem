aafec84a3aab2df5ac8c56473dbeed7f88139e03
Wojtek Rymer <wrymer2@gmail.com>
2022-10-20 09:16:18 +0200

    zad2 on

--- on/1lab/zad2.jl
+++ on/1lab/zad2.jl
@@ -3,9 +3,10 @@ using Printf
 function calceps(T)
     three::T = 3.0
     four::T = 4.0
-    i::T = three * T(T(four / three) - T(1.0)) - T(1.0)
+    i::T = T(three * T(T(four / three) - T(1.0))) - T(1.0)
     ep = eps(T)
     @printf("eps = %.e\ncalculated = %.e\n", ep, i)
     @printf("ifequal = %s\n",isequal(ep,i))
-
+    println(bitstring(ep))
+    println(bitstring(i))
 end
--- on/1lab/zad5.jl
+++ on/1lab/zad5.jl
@@ -1,5 +1,7 @@
-x = [2.718281828, -3.141592654, 1.414213562, 0.5772156649, 0.3010299957]
-y =  [1486.2497, 878366.9879, -22.37492, 4773714.647, 0.000185049]
+Ty = Float32
+
+x = [Ty(2.718281828), Ty(-3.141592654), Ty(1.414213562), Ty(0.5772156649), Ty(0.3010299957)]
+y =  [Ty(1486.2497), Ty(878366.9879), Ty(-22.37492), Ty(4773714.647), Ty(0.000185049)]
 z = [0.,0.,0.,0.,0.]
 for i in 1:5
     z[i] = x[i]*y[i]
@@ -7,16 +9,16 @@ end
 @show(z)
 
 
-function sum1(z)
-    sum::Float64 = 0.0
+function sum1(z,T)
+    sum::T = 0.0
     for i in 1:size(z)[1]
         sum+=z[i]
     end
     return sum
 end
 
-function sum2(z)
-    sum::Float64 = 0.0
+function sum2(z,T)
+    sum::T = 0.0
     s = size(z)[1]
     for i in 1:s
         sum+=z[s - i + 1]
@@ -24,30 +26,33 @@ function sum2(z)
     return sum
 end
 
-function sum3(z)
-    sump::Float64 = 0.0
-    sumn::Float64 = 0.0
+function sum3(z,T)
+    sump::T = 0.0
+    sumn::T = 0.0
     sort!(z)
+    # @show(z)
     i = 1
-    while i > size(z)[1] || z[i] < 0 
+    while i < size(z)[1] && z[i] < 0 
         sumn += z[i]
+        # @show(sumn)
         i+=1
     end
     i = size(z)[1]
-    while i > size(z)[1] && z[i] > 0 
+    while i > 0 && z[i] > 0 
         sump += z[i]
-        i+=1
+        # @show(sump)
+        i-=1
     end
     return sump  + sumn
 end
 
-function sum4(z)
-    sump::Float64 = 0.0
-    sumn::Float64 = 0.0
+function sum4(z,T)
+    sump::T = 0.0
+    sumn::T = 0.0
     sort!(z)
-    show(z)
+    # show(z)
     i = 1
-    while i >  size(z)[1] || z[i] < 0 
+    while i  <  size(z)[1] && z[i] < 0 
         i+=1
     end
     
@@ -60,7 +65,11 @@ function sum4(z)
     return sump  + sumn
 end
 
-println(sum1(z))
-println(sum2(z))
-println(sum3(z))
-println(sum4(z))
+println(sum1(z,Float32))
+println(sum1(z,Float64))
+println(sum2(z,Float32))
+println(sum2(z,Float64))
+println(sum3(z,Float32))
+println(sum3(z,Float64))
+println(sum4(z,Float32))
+println(sum4(z,Float64))
