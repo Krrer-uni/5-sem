1deb7a94f8d6613c887ef9dccff631ddc27e2353
Krrer-uni <wrymer2@gmail.com>
2022-10-26 01:37:55 +0200

    jfft

--- sp/zad2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-find $1 -type f -exec cat {} + | tr '[:space:]' '[\n*]' | sort | uniq -c $b
-# echo $b
-# echo $()
-# for c in $(uniq $b)
-# do
-#     echo $c $(wc -w $b | grep -c $c)
-# done
-# # tr '[:space:]' '[\n*]' < a/aa/b/aaab | cat aasda
