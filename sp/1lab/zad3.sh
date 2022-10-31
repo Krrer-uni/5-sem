92e4d650e0c6f831de8b2635867ec255c920fbc9
Wojtek Rymer <wrymer2@gmail.com>
2022-10-31 12:19:12 +0100

    sp

--- sp/1lab/zad3.sh
+++ sp/1lab/zad3.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
-a=$(find $1 -type f -exec cat {} \; | tr '[:space:]' '[\n*]' | uniq )
-echo $a | tr '[:space:]' '[\n*]' | sort | uniq
+a=$( find $1 -type f )
+text=""
+for file in $a 
+do
+    ap=$( cat $file |  tr '[:space:]' '[\n*]'| sort | uniq )
+    text+=" $ap" 
+done
+echo $text | tr '[:space:]' '[\n*]' | sort | uniq -c 
