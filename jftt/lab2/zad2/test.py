394d0b44c858e6aa54bd0f81a94c2ec95193b52e
Krrer-uni <wrymer2@gmail.com>
2022-11-09 13:55:31 +0100

    jftt, this time whole

---
+++ jftt/lab2/zad2/test.py
@@ -0,0 +1,16 @@
+#This is a comment
+#written in
+#more than just one line
+print("Hello, World!")
+"""
+This is a comment
+writte"n in
+more th""an just one line
+"""
+print("This should be printed")
+"""
+asdas
+"""
+print("Hello, World!")
+#print("Hello, World!")
+print("Cheers, Mate!")
