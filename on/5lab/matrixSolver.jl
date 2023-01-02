e40c754aa2dcc0cbeb9146169dbdf570069d975b
Krrer-uni <wrymer2@gmail.com>
2023-01-03 00:23:27 +0100

    partial choice not working

--- on/5lab/matrixSolver.jl
+++ on/5lab/matrixSolver.jl
@@ -51,3 +51,56 @@ function solveGaussForx(inputA, inputb)
     
     return x
 end
+
+function solveGaussForxParttialChoice(inputA, inputb)
+    A = transpose(copy(inputA[1]))
+    n = copy(inputA[2])
+    l = copy(inputA[3])
+    b = copy(inputb)
+    x = zeros(n)
+
+    rowlookup = collect(1:n)
+
+    for i in 1:n-1 #column
+        firstRow = min(i+1,n)
+        lastRow = min(i+l, n)
+        t = i
+        max = A[i,i]
+        for k in firstRow:lastRow #row
+            coef = A[i,k] 
+            if coef > max
+                max = coef
+                t = k
+            end
+    
+        end
+        A[:,t],A[:,i] = A[:,i],A[:,t]
+        b[t],b[i] = b[i],b[t]
+        # println(b)
+        rowEnd = min(i+2*l,n)
+        for k in firstRow:lastRow #row
+            if A[i,k] == 0
+                continue
+            end
+            q = -A[i,k]/A[i,i]
+            A[i:rowEnd,k] = A[i:rowEnd,k] + A[i:rowEnd,i] * q
+            A[i,k] = 0
+            b[k] = b[k] + b[i] * q
+        end
+    end
+    
+    A = transpose(A)
+    dropzeros!(A)
+    x[n] = b[n] / A[n,n]
+
+    for i in (n-1):-1:1
+        rowEnd = min(i+l,n)
+        
+        for k in rowEnd:-1:i+1
+            b[i] = b[i] - A[i,k] * x[k]
+        end
+        x[i] = b[i] / A[i,i]
+    end
+    
+    return x
+end
--- on/5lab/test.jl
+++ on/5lab/test.jl
@@ -1,5 +1,6 @@
 include("matrixSolver.jl")
 include("reader.jl")
+using Statistics
 
 # A = loadMatrix("Dane16_1_1/A.txt")
 # b = loadVector("Dane16_1_1/b.txt")
@@ -7,8 +8,9 @@ include("reader.jl")
 
 # println(solveGaussForb(A,x))
 # solveGaussForx(A,b)
-A = loadMatrix("Dane500000_1_1/A.txt")
-b = loadVector("Dane500000_1_1/b.txt")
-x = ones(Float64,A[2])
-@time solveGaussForb(A,x)
-@time solveGaussForx(A,b)
+A = loadMatrix("test/Dane10000_1_1/A.txt")
+b = loadVector("test/Dane10000_1_1/b.txt")
+x = ones(Float64,A[2]) * 2
+@time b = solveGaussForb(A,x)
+println(mean(map(abs,solveGaussForx(A,b) - x)))
+println(solveGaussForxParttialChoice(A,b) - x)
